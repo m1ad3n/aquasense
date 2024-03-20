051f8ea26e0f46368d050fb7693da1c92ecad62f
Mladen <stanimiroviccmladen@gmail.com>
2024-03-20 13:44:23 +0000

    initilaized glew and added simple in-file documentation

--- src/aquasense.c
+++ src/aquasense.c
@@ -1,36 +1,65 @@
+/**
+  * @author Mladen Stanimirovic
+  * @file aquasense.c
+  * @brief Main C source file for this project
+  */
+
 #include "deps.h"
 #include "macros.h"
 #include <GLFW/glfw3.h>
 
+/**
+ * @brief      Error callback function for GLFW
+ *
+ * @param[in]  error        The error
+ * @param[in]  description  The description
+ */
 static void error_callback(int error, const char* description)
 {
     fprintf(stderr, "GLFW ERROR: %s\n", description);
 }
 
 int main(int argc, char *argv[]) {
-
     glfwSetErrorCallback(error_callback);
 
     if (!glfwInit())
         return -1;
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Basic OpenGL Window", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "AquaSense", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
 
-    // application loop (prevents window from closing)
+    // initializing opengl (glew)
+    int error_code = glewInit();
+    if (error_code != GLEW_OK) {
+        fprintf(stderr, "OPENGL ERROR: something wrong with glewInit()\n");
+        goto glfw_cleanup_and_exit;
+    }
+
+    // details about opengl
+    printf("OpenGL Version %s\n", glGetString(GL_VERSION));
+
+    // main application loop
     while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(1.0, 1.0, 1.0, 1.0);
+
+        // glfw stuff
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+glfw_cleanup_and_exit:
     // glfw memory cleanup 
     glfwDestroyWindow(window);
     glfwTerminate();
 
-    // 0
+    // message for developers
+    printf("GLFW: Memory successfully released\n");
+
+    // exit status
     return EXIT_SUCCESS;
 }
--- src/deps.h
+++ src/deps.h
@@ -1,14 +1,20 @@
-#ifndef AQUASENSE_DEPS_HEADER
-#define AQUASENSE_DEPS_HEADER
+/**
+ * @file deps.h
+ * 
+ * @brief Header file containing dependencies
+ */
 
-// opengl
+#ifndef AQUASENSE_DEPS
+#define AQUASENSE_DEPS
+
+// opengl (must be included before any other opengl library's)
 #include <GL/glew.h>
 
 // glfw
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-// standard library for c
+// standard library's for c
 #include <stdio.h>
 #include <stdlib.h>
 
